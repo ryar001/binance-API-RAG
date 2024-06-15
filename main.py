7af784fc5fc795b5730fdee21931aa4e8f5ce6bf
ryar001 <57259642+ryar001@users.noreply.github.com>
2024-06-15 16:51:19 +0800

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+import getpass
+import os
+import dotenv
+
+dotenv.load_dotenv(dotenv_path=".env")  # Specify the path to your .env_ai file
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY", default=getpass.getpass())
